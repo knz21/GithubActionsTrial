b499a84f556fe70fbeafc64692b9ee68f9ab4bbd
knz21 <cismollre@gmail.com>
2022-07-02 10:38:17 +0900

    fixed

--- app/src/test/java/com/knz21/githubactionstrial/ExampleUnitTest.kt
+++ app/src/test/java/com/knz21/githubactionstrial/ExampleUnitTest.kt
@@ -14,14 +14,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun addition_isNotCorrect() {
-        assertEquals(3, 2 + 2)
-    }
-
-    @Test
-    fun addition_isNotCorrect2() {
-        assertEquals(5, 2 + 2)
-    }
 }
